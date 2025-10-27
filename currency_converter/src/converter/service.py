fbbee25714b2200d84beee556af9105a7a86e1f8
Mishanya00 <polyd@tut.by>
2025-10-27 12:57:29 +0300

    fix: CurrencyConverterService

--- currency_converter/src/converter/service.py
+++ currency_converter/src/converter/service.py
@@ -2,7 +2,7 @@ from src.config import settings
 from src.client.currency_rate_client import CurrencyRateClient
 
 
-class CurrencyConverterService():
+class CurrencyConverterService:
     async def get_currency_rates(period: int = 0):
         async with CurrencyRateClient(base_url=settings.EXTERNAL_API_URL) as aclient:
             response = await aclient.get_rates(period)
