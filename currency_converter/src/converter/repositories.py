373a7de0abbd7c587af3e98ae053e682ac6757b4
Mishanya00 <polyd@tut.by>
2025-10-29 18:23:26 +0300

    feat: new repository queries. Refactoring

--- currency_converter/src/converter/repositories.py
+++ currency_converter/src/converter/repositories.py
@@ -1,5 +1,4 @@
 from datetime import date
-from typing import List
 
 from sqlalchemy import select, insert
 
@@ -9,7 +8,7 @@ from src.converter.models import ExchangeRate
 
 
 class ExchangeRateRepository(BaseRepository):
-    async def is_present_by_date(self, cur_date: date):
+    async def is_present_by_date(self, cur_date: date) -> bool:
         result = await self.session.execute(
             select(ExchangeRate).filter(ExchangeRate.cur_date == cur_date)
         )
@@ -17,20 +16,42 @@ class ExchangeRateRepository(BaseRepository):
             return True
         return False
 
-    async def select_rates_by_cur_date(self, cur_date: date) -> List[ExchangeRate]:
+    async def select_rates_by_cur_date(self, cur_date: date) -> list[ExchangeRate]:
         result = await self.session.execute(
             select(ExchangeRate).filter(ExchangeRate.cur_date == cur_date)
         )
         return list(result.scalars().all())
 
-    async def insert_many_rates(self, rates: List[ExchangeRateBaseSchema]):
+    async def select_rate_by_cur_abbreviation(
+        self, cur_abbreviation: str
+    ) -> ExchangeRate:
+        result = await self.session.execute(
+            select(ExchangeRate).filter(
+                ExchangeRate.cur_abbreviation == cur_abbreviation
+            )
+        )
+        return result.scalar_one_or_none()
+
+    async def select_two_rates_by_cur_abbreviations(
+        self, source_cur_abbreviation, target_cur_abbreviation
+    ) -> list[ExchangeRate]:
+        result = await self.session.execute(
+            select(ExchangeRate).filter(
+                (ExchangeRate.cur_abbreviation == source_cur_abbreviation)
+                | (ExchangeRate.cur_abbreviation == target_cur_abbreviation)
+            )
+        )
+
+        return list(result.scalars().all())
+
+    async def insert_many_rates(self, rates: list[ExchangeRateBaseSchema]):
         if not rates:
             return []
 
         values_to_insert = [rate.model_dump() for rate in rates]
-
-        statement = insert(ExchangeRate).values(values_to_insert).returning(ExchangeRate)
-
+        statement = (
+            insert(ExchangeRate).values(values_to_insert).returning(ExchangeRate)
+        )
         result = await self.session.execute(statement)
 
         inserted_rows = list(result.scalars().all())
