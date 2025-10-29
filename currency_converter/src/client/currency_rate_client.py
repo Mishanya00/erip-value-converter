a210890c8545c6aced523645ff2a438733ad4d62
Mishanya00 <polyd@tut.by>
2025-10-30 01:21:20 +0300

    feat: fetch API data directly by ondate because National Bank API updates its data with a little delay

--- currency_converter/src/client/currency_rate_client.py
+++ currency_converter/src/client/currency_rate_client.py
@@ -1,13 +1,36 @@
+from datetime import date
+
 from src.client.schemas import ExternalAPIExchangeRateSchema
 from src.client.http_client import BaseHTTPClient
 
 
 class CurrencyRateClient(BaseHTTPClient):
-    async def get_rates(self, periodicity: int = 0) -> list[ExternalAPIExchangeRateSchema]:
+    async def get_rates(
+        self, periodicity: int = 0
+    ) -> list[ExternalAPIExchangeRateSchema]:
         response = await self.get("/exrates/rates", params={"periodicity": periodicity})
         response.raise_for_status()
         raw_rates_list = response.json()
 
-        validated_rates = [ExternalAPIExchangeRateSchema.model_validate(item) for item in raw_rates_list]
+        validated_rates = [
+            ExternalAPIExchangeRateSchema.model_validate(item)
+            for item in raw_rates_list
+        ]
+
+        return validated_rates
+
+    async def get_rates_by_date(
+        self, ondate: date, periodicity: int = 0
+    ) -> list[ExternalAPIExchangeRateSchema]:
+        response = await self.get(
+            "/exrates/rates", params={"periodicity": periodicity, "ondate": ondate}
+        )
+        response.raise_for_status()
+        raw_rates_list = response.json()
+
+        validated_rates = [
+            ExternalAPIExchangeRateSchema.model_validate(item)
+            for item in raw_rates_list
+        ]
 
         return validated_rates
