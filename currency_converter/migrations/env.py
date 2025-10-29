754f14db70a90c4e5c9f7062fc529cad28aafb83
Mishanya00 <polyd@tut.by>
2025-10-29 19:56:02 +0300

    feat: new Exchange database model.

--- currency_converter/migrations/env.py
+++ currency_converter/migrations/env.py
@@ -12,7 +12,7 @@ from alembic import context
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.config import settings
 from src.repository.database import Base
-from src.converter.models import ExchangeRate  # noqa F401
+from src.converter.models import ExchangeRate, Exchange  # noqa F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- currency_converter/src/converter/models.py
+++ currency_converter/src/converter/models.py
@@ -7,7 +7,7 @@ from sqlalchemy import text, UUID, Integer, DateTime, Date, Numeric
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.config import settings
-from src.custom_types import Str3, Str128
+from src.custom_types import Str3, Str128, ExchangeStatus
 from src.repository.database import Base
 
 
@@ -52,4 +52,26 @@ class ExchangeRate(Base):
         return (
             f"<ExchangeRate: {self.cur_id} - {self.cur_abbreviation} - {self.cur_scale} - "
             f"{self.cur_name} - {self.cur_official_rate}>"
+        )
+
+
+class Exchange(Base):
+    __tablename__ = "exchange"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+
+    source_cur_abbreviation: Mapped[Str3] = mapped_column(nullable=False)
+    target_cur_abbreviation: Mapped[Str3] = mapped_column(nullable=True)
+    source_amount: Mapped[Decimal] = mapped_column(Numeric(19, 4), nullable=False)
+    target_amount: Mapped[Decimal] = mapped_column(Numeric(19, 4), nullable=False)
+    rate: Mapped[Decimal] = mapped_column(Numeric(19, 28), nullable=False)
+    status: Mapped[ExchangeStatus] = mapped_column(nullable=False)
+
+    created_at: Mapped[created_at_type]
+    modified_at: Mapped[modified_at_type]
+
+    def __repr__(self) -> str:
+        return (
+            f"<Exchange {self.id}: {self.source_cur_abbreviation} -> {self.target_cur_abbreviation}:"
+            f"{self.source_amount} -> {self.target_amount}. {self.rate} - {self.status}>"
         )
